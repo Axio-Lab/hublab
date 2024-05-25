6385523aa21f3fbb75f2b94e5a88eb3c6fd673f0
fathom329 <chuksaginamada@gmail.com>
2024-05-25 12:20:17 +0100

    Feat: Implement payment endpoint with candypay

---
+++ backened/src/controllers/payment.controller.ts
@@ -0,0 +1,45 @@
+import { Request, Response } from "express";
+import PaymentService from "../services/payment.service";
+import axios from "axios";
+const { createCandypaySession, generatePaymentURL } = new PaymentService();
+
+export default class PaymentController {
+
+    async createPayment(req: Request, res: Response) {
+        const candypaySession = await createCandypaySession();
+
+        return res.status(200)
+            .send({
+                success: true,
+                message: "Payment session created successfully",
+                session: candypaySession
+            })
+    }
+
+    async createPaymentAPI(req: Request, res: Response) {
+        
+        const sessionId = await axios.post("https://checkout-api.candypay.fun/api/v1/session", {
+            network: "devnet",
+            success_url: "https://www.google.com",
+            cancel_url: "https://www.jumia.ng",
+            tokens: ["bonk"],
+            items: [
+                {
+                    name: "Throwback Hip Bag",
+                    price: 0.0001,
+                    image: "https://imgur.com/EntGcVQ.png",
+                    quantity: 1
+                }
+            ]
+        }, {
+            headers: { Authorization: `Bearer cp_public_2jC3uB4z_S69sXDksX5XpibEQ1vhhdJJD` }
+        })
+        return res.status(200)
+            .send({
+                success: true,
+                message: "Payment sessionId created successfully",
+                sessionId
+            })
+    }
+
+}
--- backened/src/routes/index.routes.ts
+++ backened/src/routes/index.routes.ts
@@ -1,5 +1,6 @@
 import profileRouter from "./profile.routes";
 import campaignRouter from "./campaign.routes";
+import paymentRouter from "./payment.route";
 import docRouter from "./doc.routes";
 import { basePath } from "../configs/constants.configs";
 import { Request, Response } from "express";
@@ -7,6 +8,7 @@ import { Request, Response } from "express";
 export default (app: { use: (arg0: string, arg1: any) => void; }) => {
     app.use(`${basePath}/profiles`, profileRouter);
     app.use(`${basePath}/campaigns`, campaignRouter);
+    app.use(`${basePath}/payment`, paymentRouter);
     app.use(`${basePath}/docs`, docRouter);
     app.use(`${basePath}/`, (req: Request, res: Response) => {
         res.send("Welcome to Verxio API");
